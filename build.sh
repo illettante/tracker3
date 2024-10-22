dbc13238b82ee9490df4e585e83c4998e616faed
illettante <94096440+illettante@users.noreply.github.com>
2024-10-22 13:46:48 +1100

    Update build.sh to delete class files after build
    just tidying up

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@ javac -verbose -d build --release 8 src/Tracker3.java src/ModPlay3.java
 cd build
 touch MANIFEST.MF
 echo "Main-Class: Tracker3" > MANIFEST.MF
-jar cfmv tracker3.2.jar MANIFEST.MF Tracker3.class ModPlay3.class
+jar cfmv tracker3.3.jar MANIFEST.MF Tracker3.class ModPlay3.class
+cd ..
+rm -rf src/*.class
+rm -rf build/*.class
+rm -rf build/MANIFEST.MF
+echo done
